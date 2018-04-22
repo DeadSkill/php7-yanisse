4974ec6395f01b4cf3290eca775bd78b09a622da
DeadSkill <yanisse.youmi@ynov.com>
2018-04-22 23:58:13 +0200

    .

--- 01-script-php/insultron_exo.php
+++ 01-script-php/insultron_exo.php
@@ -1,37 +1,17 @@
 <?php
 
-$tabaleatoire = [
-        "super",
-        "bon",
-        "rapide",
-        "delicieux",
-        "magnifique",
-];
-$tabaleatoire2 = [
-        "developpeur",
-        "cuisinier",
-        "narcotranficant",
-        "killeur",
-        "chanteur",
-];
-$tabaleatoire3 = [
-        "php",
-        "c++",
-        "tacos",
-        "cocaine",
-];
-$tabaleatoire4 = [
-        "felicite",
-        "",
-];
+$random = mt_rand (0, 5);
+$random2 = mt_rand (0, 4);
+$random3 = mt_rand (0, 3);
+$random4 = mt_rand (0, 3);
 
-$index = array_rand ($tabaleatoire [ $num = 1])
-$resultat = array_merge ($tabaleatoire, $tabaleatoire2, $tabaleatoire3, $tabaleatoire4);
+$static = "Tu es un ";
+$static2 = "de ";
+$static3 = "en terme de ";
 
-)
-$citation = "Tu es un $tabaleatoire[$index] $tabaleatoire2[$index] $tabaleatoire3[$index], je te $tabaleatoire4[$index] pour ce très beau script";
+$complimentStep1 = ['beau ', 'gentil ', 'super ', 'génial ', "bon ", "magnifique "];
+$complimentStep2 = ["animal ", "joueur ", "developpeur", "defenseur ", "monstre"];
+$complimentStep3 = [ "la jungle ", "php", "call of duty", "football "];
+$complimentStep4 = [ "skill." , "popularité.", "bambi attitude.", "hippie"];
 
-print "$citation";
-
-
-?>
+$display =  $static . $complimentStep1[$random] . $complimentStep2[$random2]. $static2 . $complimentStep3[$random3] . $static3 . $complimentStep4[$random4]."\n";
